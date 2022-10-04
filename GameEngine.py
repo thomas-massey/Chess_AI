e4969dc277fabae3f3ee295ac3372b446ff4e341
Leftmostchain21 <thomassey476@outlook.com>
2022-10-04 09:00:35 +0100

    Added full support for random moves

--- GameEngine.py
+++ GameEngine.py
@@ -15,28 +15,19 @@ class Game:
         self.board = chess.Board()
         # We create a new render.
         self.render = Render.render(self.board)
-        while True:
-            player_colour = input("What colour would you like to play as, White, Black or Random? (w/b/r): ")
-            if player_colour == "w":
-                player_colour = "White"
-                break
-            elif player_colour == "b":
-                player_colour = "Black"
-                break
-            elif player_colour == "r":
-                type = randint(0, 1)
-                if type == 0:
-                    player_colour = "White"
-                else:
-                    player_colour = "Black"
+        # Use the screen for getting who goes first
+        player_colour = self.render.get_turn()
+        # Load the graphics
+        self.render.load_graphics()
         # Game loop
         while self.board.is_checkmate != False:
             # Find who's turn it is.
-            if self.board.turn == True:
-                turn = "White"
-            else:
-                turn = "Black"
-            if turn == player_colour:
+            if not player_colour == "AIBattle":
+                if self.board.turn == True:
+                    turn = "White"
+                else:
+                    turn = "Black"
+            if ((player_colour == "Black") or (player_colour == "White")) and (player_colour == turn):
                 legal_moves = self.board.generate_legal_moves()
                 for move in legal_moves:
                     print(move)
@@ -44,6 +35,7 @@ class Game:
                 converted_move = self.render.get_events(self.board, turn)
             else:
                 # Basic form of random AI.
+                ai_data = self.generate_ai_data()
                 # Get a list of all the legal moves.
                 moves = []
                 legal_moves = self.board.generate_legal_moves()
@@ -59,13 +51,256 @@ class Game:
             potencial_move = chess.Move.from_uci(converted_move)
             # If the move is legal, then we make the move.
             sleep(0.5)
+            # Get the turn
+            if self.board.turn == True:
+                turn = "White"
+            else:
+                turn = "Black"
             if self.board.is_legal(potencial_move):
                 print(turn + ": Move is legal")
                 self.board.push(potencial_move)
-                self.render.update_board(self.board, turn)
+                self.render.update_board(self.board, turn, converted_move)
             else:
                 print(turn + ": Move is not legal")
 
-
+    def generate_ai_data(self):
+        # First feed it some data:
+        # The format should always be a  array of 0's and 1's.
+        # We will use the following format:
+        # 0 = Empty
+        # 1 = Occupied
+        white_pawns = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("P"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_pawns.append(temp_row)
+            temp_row = []
+        black_pawns = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("p"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_pawns.append(temp_row)
+            temp_row = []
+        white_rooks = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("R"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_rooks.append(temp_row)
+            temp_row = []
+        black_rooks = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("r"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_rooks.append(temp_row)
+            temp_row = []
+        white_knights = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("N"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_knights.append(temp_row)
+            temp_row = []
+        black_knights = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("n"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_knights.append(temp_row)
+            temp_row = []
+        white_bishops = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("B"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_bishops.append(temp_row)
+            temp_row = []
+        black_bishops = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("b"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_bishops.append(temp_row)
+            temp_row = []
+        white_queens = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("Q"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_queens.append(temp_row)
+            temp_row = []
+        black_queens = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("q"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_queens.append(temp_row)
+            temp_row = []
+        white_kings = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("K"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            white_kings.append(temp_row)
+            temp_row = []
+        black_kings = []
+        for j in range(1, 9):
+            temp_row = []
+            original_j = j
+            for i in range(1, 9):
+                # # Invert j so 8 is 1 and 1 is 8
+                j = 9 - original_j
+                # Convert the i to a letter.
+                letter = chr(i + 96)
+                if self.board.piece_at(chess.parse_square(str(letter) + str(j))) == chess.Piece.from_symbol("k"):
+                    # Append 1
+                    temp_row.append("1")
+                else:
+                    # Append 0
+                    temp_row.append("0")
+            black_kings.append(temp_row)
+            temp_row = []
+        if self.board.is_check:
+            in_check = 1
+        else:
+            in_check = 0
+        if self.board.has_kingside_castling_rights(chess.WHITE):
+            white_kingside_castle = 1
+        else:
+            white_kingside_castle = 0
+        if self.board.has_queenside_castling_rights(chess.WHITE):
+            white_queenside_castle = 1
+        else:
+            white_queenside_castle = 0
+        if self.board.has_kingside_castling_rights(chess.BLACK):
+            black_kingside_castle = 1
+        else:
+            black_kingside_castle = 0
+        if self.board.has_queenside_castling_rights(chess.BLACK):
+            black_queenside_castle = 1
+        else:
+            black_queenside_castle = 0
+        if self.board.has_legal_en_passant():
+            en_passant_possible = 1
+        else:
+            en_passant_possible = 0
+        # Define endgame states
+        is_draw = ""
+        # Now print all the data.
+        print("White pawns: \n" + str(white_pawns) + "\nBlack pawn: \n" + str(black_pawns) + "\nWhite rooks: \n" + str(white_rooks) + "\nBlack rooks: \n" + str(black_rooks) + "\nWhite knights: \n" + str(white_knights) + "\nBlack knights: \n" + str(black_knights) + "\nWhite bishops: \n" + str(white_bishops) + "\nBlack bishops: \n" + str(black_bishops) + "\nWhite queens: \n" + str(white_queens) + "\nBlack queens: \n" + str(black_queens) + "\nWhite kings: \n" + str(white_kings) + "\nBlack kings: \n" + str(black_kings))
+        
     def get_legal_moves(self):
         return self.board.legal_moves
